bfa703d3fd23d42cb470f8a0a053b5f3f9a486c9
luccab <luccabazzo@gmail.com>
2018-11-10 19:15:59 +0900

    allowing user to select breed and loading image retrieved by the API

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -4,11 +4,18 @@ class IndexController < ActionController::Base
 
     require 'rest-client'
     require 'json'
+    @q = "#{params[:breeds]}"
+    all_breeds = JSON.parse(RestClient.get "https://dog.ceo/api/breeds/list/all")
+    @list = all_breeds["message"].keys
 
     #url = "https://dog.ceo/dog-api/documentation/random"
     #response = RestClient.get(url)
-    response = JSON.parse(RestClient.get "https://dog.ceo/api/breeds/image/random")
-    @this = response["message"]
+    if !@q.blank?
 
+      response = JSON.parse(RestClient.get "https://dog.ceo/api/breed/#{params[:breeds]}/images")
+      @this = response["message"].sample
+    end
   end
+
+
 end
